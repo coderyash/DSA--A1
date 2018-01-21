ccfae358ae31b91bfff387786c755aa39776e33b
coderyash <coderyash@github.com>
2018-01-21 18:00:14 +0530

    new

---
+++ large_factorial/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int multiply(int i,int arr_size, int max[])
+{
+  int carry = 0;
+  for(j=0;j<arr_size;j++)
+  {
+    max[i] = ()
+  }
+}
+void factorial(int* max, int num)
+{
+  int arr_size = 1;
+  max[0] = 1;
+  for(i=2;i<=num;i++)
+  {
+    arr_size = multiply(i,arr_size,max);
+  }
+
+}
+int main()
+{
+  int max[1000];
+  int num;
+  scanf("%d",&num);
+  factorial(max,num);
+}
